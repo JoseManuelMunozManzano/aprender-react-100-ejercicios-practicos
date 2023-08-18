b17807787e93fab9b9e9696a1addd320ffcb959c
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-08-18 20:19:38 +0200

    16-Problema 4 - Fetching data - Solución

--- 16-useEffect-problemas-tipicos/src/components/04-fetching-data/Posts.tsx
+++ 16-useEffect-problemas-tipicos/src/components/04-fetching-data/Posts.tsx
@@ -9,6 +9,8 @@ export const Posts = () => {
   const [posts, setPosts] = useState<Post[]>([]);
 
   useEffect(() => {
+    let isCancelled = false;
+
     // Tenemos un router cuyo Home es: http://localhost:5173/
     // Cuando pulsamos en Go to posts vamos a: http://localhost:5173/posts
     // y se ejecuta este componente.
@@ -20,14 +22,27 @@ export const Posts = () => {
     // Ahora pulsamos en Go to posts, pero rápidamente pulsamos en el botón para retroceder la página.
     // Vemos que, aunque hemos retrocedido, sale el alert y actualiza el state.
     // Pero debería haberse cancelado esta petición inmediatamente tras pulsar el botón retroceder.
+    //
+    //? SOLUCION
+    //? Necesitamos una función de limpieza. Esto arreglaría un caso tan simple como este, pero para
+    //? casos con más estados tendremos problemas más grandes.
+    //?
+    //? Una estrategia muy común es crear una variable booleana (isCancelled).
+    //? Mientras su valor sea false actualizaremos el estado y cuando queramos cancelar, será true.
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then((res) => res.json())
       .then((data: Post[]) => {
-        alert('posts are ready, updating state!');
-        setPosts(data);
-        console.log(data);
+        if (!isCancelled) {
+          alert('posts are ready, updating state!');
+          setPosts(data);
+          console.log(data);
+        }
       })
       .catch(console.error);
+
+    return () => {
+      isCancelled = true;
+    };
   }, []);
 
   return (
