8745e5a101771cf162bb9f5f13e2432563dcc4e8
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-08-18 18:59:01 +0200

    16-Problema 3 - Updating useEffect - Explicamos el problema

--- 16-useEffect-problemas-tipicos/src/components/03-updating-use-effect/UpdatingUseEffect.tsx
+++ 16-useEffect-problemas-tipicos/src/components/03-updating-use-effect/UpdatingUseEffect.tsx
@@ -4,11 +4,14 @@ export const UpdatingUseEffect = () => {
   const [number, setNumber] = useState<number>(0);
 
   useEffect(() => {
-    // Este console.log es un bucle infinito
+    // Este console.log es un bucle infinito.
     console.log('effect');
 
     // Si ejecutamos este componente en el navegador veremos que pasa algo raro.
     // Ocurre un extraño glitch.
+
+    // Este error ocurre porque number cambia cada segundo y como es nuestra dependencia
+    // ejecuta este useEffect sin parar.
     setInterval(() => {
       setNumber(number + 1);
     }, 1000);
