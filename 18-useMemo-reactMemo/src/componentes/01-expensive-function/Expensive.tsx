929d4da34613b7f6369d399d0e54d43f0d3d31b2
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-08-20 05:30:25 +0200

    18-useMemo - Solución

--- 18-useMemo-reactMemo/src/componentes/01-expensive-function/Expensive.tsx
+++ 18-useMemo-reactMemo/src/componentes/01-expensive-function/Expensive.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
 export const Expensive = () => {
   const [text, setText] = useState<string>('');
@@ -15,7 +15,18 @@ export const Expensive = () => {
   // Cada vez que actualizo el texto el componente se renderiza.
   // Problema: Como al re-renderizar ejecuta esta función pesada, cada vez que actualizo el texto
   //    hay un delay para mostrar el nuevo texto escrito.
-  const sum = expensiveFunction();
+  //
+  //? SOLUCION:
+  //? No tiene sentido, en cada renderizado, volver a llamar a esa función tan pesada, porque el resultado
+  //? de la función no va a cambiar nunca.
+  //? Es en estos escenarios donde tiene todo el sentido usar el hook useMemo.
+  //? useMemo toma como argumentos un callback y un array de dependencias donde indicamos lo que cambia para
+  //? que vuelva a ejecutarse la función pensada. En este caso, como el total devuelto no cambia nuca, está
+  //? vacío.
+  //?
+  //? Tras este cambio, si ejecutamos de nuevo la app, veremos que ya no existe ese delay al actualizar
+  //? la caja de texto.
+  const sum = useMemo(() => expensiveFunction(), []);
   console.log('Componente re-rendered!');
 
   return (
