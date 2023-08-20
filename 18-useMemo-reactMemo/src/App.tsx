13678b186d8a8ca874afd320d9a1f1190ade3d9f
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-08-20 05:55:22 +0200

    18-useMemo - Objetos - Indicación del problema

--- 18-useMemo-reactMemo/src/App.tsx
+++ 18-useMemo-reactMemo/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
-import { /* Expensive, */ ExpensiveWithDependency } from './componentes/01-expensive-function';
+// import { Expensive, ExpensiveWithDependency } from './componentes/01-expensive-function';
+import { WithObject } from './componentes/02-with-objects-useEffect/WithObject';
 
 function App() {
   return (
     <>
       {/* <Expensive /> */}
-      <ExpensiveWithDependency />
+      {/* <ExpensiveWithDependency /> */}
+      <WithObject />
     </>
   );
 }
---
+++ 18-useMemo-reactMemo/src/componentes/02-with-objects-useEffect/WithObject.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+
+export const WithObject = () => {
+  const [name, setName] = useState<string>('');
+  const [age, setAge] = useState<number | null>(null);
+  const [country, setCountry] = useState<string>('');
+
+  // PROBLEMA:
+  // Este calculo no es pesado, por lo que en principio no haría falta useMemo y todo está bien.
+  // Además userType depende de dos estados, por lo que se va a actualizar muy a menudo.
+  //
+  // El problema es que userType es un objeto, y aquí enlazamos con el proyecto 16-useEffect-problemas-tipicos
+  // cuando hablábamos sobre la diferencia entre primitivos y no primitivos.
+  // Cuando se renderiza el componente, este objeto no será el mismo que en la renderización anterior.
+  //
+  // Esto significa que, incluso aunque cambie name, y userType NO TIENE NADA QUE VER CON NAME, se va a volver
+  // a renderizar porque la dirección de memoria de userType entre renderizados cambia porque userType se vuelve
+  // a asignar!!
+  const userType = {
+    underAge: age! < 18 ? true : false,
+    citizen: country === 'USA' ? true : false,
+  };
+
+  useEffect(() => {
+    console.count('userType ha cambiado!');
+  }, [userType]);
+
+  console.log('Componente renderizado!');
+
+  return (
+    <div>
+      <input type="text" onChange={(e) => setName(e.target.value)} placeholder="name" />
+      <br />
+      <input onChange={(e) => setAge(+e.target.value)} placeholder="age" type="number" />
+      <br />
+      <select onChange={(e) => setCountry(e.target.value)}>
+        <option value="USA">USA</option>
+        <option value="USA">COL</option>
+        <option value="USA">ESP</option>
+      </select>
+    </div>
+  );
+};
